a38267f075e88ec32ce3eb5720d6661a1e0fcc0d
Arya Saputra <m.aryayudha.s@gmail.com>
2025-04-17 09:49:45 +0700

    switch build config into demo

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-const env = loadEnv('production', process.cwd(), "");
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          // isCustomElement: (tag) => tag.includes('greet-btn'),
-          isCustomElement: () => false
-        }
-      },
-      features: {
-        customElement: false
-      }
-    }),
+    vue(),
     vueJsx(),
-    vueDevTools(),
   ],
-  define: {
-    "process.env": env,
-  },
-  build: {
-    minify:false,
-    lib: {
-      entry: './src/main.web.ts',
-      name: 'greet-btn',
-      // the proper extensions will be added
-      fileName: 'greeter-btn'
-    }
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
